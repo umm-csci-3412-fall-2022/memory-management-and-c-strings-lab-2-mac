c895fd8c9a81a90dac0c34dcc33d068a5818f543
Conner <hetti031@morris.umn.edu>
2022-09-27 16:47:23 -0500

    Attempt to make disemvowel. c
    Doesn't currently work, but it should be a good start. We just have to figure out how to do the character comparisons to decide when we ignore certain characters.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,40 @@
 #include <stdlib.h>
+#include <string.h>
+
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+
+int vowelCounter(char const *str){
+        int i, numVowels;
+        for(i=0; i<strlen(str); ++i){
+                if(strcasecmp(str[i], 'a') == 0  || strcasecmp(str[i], 'e') == 0  || strcasecmp(str[i], 'i') == 0  || strcasecmp(str[i], 'o') == 0  || strcasecmp(str[i], 'u') == 0){
+                        ++numVowels;
+                }
+        }
+
+return numVowels;
+
+}
+
+
+
+char *disemvowel(char const *str) {
+  int len,numVowels,i,x;
+  char *result;
+
+  numVowels = vowelCounter(str);
+  len = strlen(str);
+  result = (char*) calloc(len+1-numVowels, sizeof(char));
+
+
+  for (i=0; i<len; ++i) {
+	if(strcasecmp(str[i], 'a') != 0 || strcasecmp(str[i], 'e') != 0 || strcasecmp(str[i], 'i') != 0  || strcasecmp(str[i], 'o') != 0  || strcasecmp(str[i], 'u') != 0){
+			result[x] = str[i];
+			++x;
+	}
+  }
+
+
+	return result;
 }
